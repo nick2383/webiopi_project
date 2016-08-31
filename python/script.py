aba795214fc455d1b0d330465a1c27bc44b4f2a5
nick2383 <nicholas.birch2@gmail.com>
2016-08-30 17:27:00 -0700

    added relay and motor control

--- python/script.py
+++ python/script.py
@@ -1,64 +1,101 @@
 import webiopi
-import datetime
+import serial
+import struct
+import time
+from webiopi.devices.serial import Serial
 
 GPIO = webiopi.GPIO
 
-LIGHT = 17 # GPIO pin using BCM numbering
+RELAY1 = 26 # GPIO pin for RELAY1 using BCM numbering
+RELAY2 = 19 # GPIO pin for RELAY2 using BCM numbering
+RELAY3 = 13 # GPIO pin for RELAY3 using BCM numbering
+RELAY4 = 6 # GPIO pin for RELAY4 using BCM numbering
 
-HOUR_ON  = 8  # Turn Light ON at 08:00
-HOUR_OFF = 18 # Turn Light OFF at 18:00
+port = serial.Serial("/dev/ttyACM0", baudrate=115200, timeout=0.1)
+checksum = 0
 
 # setup function is automatically called at WebIOPi startup
 def setup():
-    # set the GPIO used by the light to output
-    GPIO.setFunction(LIGHT, GPIO.OUT)
-
-    # retrieve current datetime
-    now = datetime.datetime.now()
-
-    # test if we are between ON time and tun the light ON
-    if ((now.hour >= HOUR_ON) and (now.hour < HOUR_OFF)):
-        GPIO.digitalWrite(LIGHT, GPIO.HIGH)
+    # set the GPIO used by the relays to output
+    GPIO.setFunction(RELAY1, GPIO.OUT)
+    GPIO.setFunction(RELAY2, GPIO.OUT)
+    GPIO.setFunction(RELAY3, GPIO.OUT)
+    GPIO.setFunction(RELAY4, GPIO.OUT)
+    GPIO.digitalWrite(RELAY1, GPIO.LOW)
+    GPIO.digitalWrite(RELAY2, GPIO.LOW)
+    GPIO.digitalWrite(RELAY3, GPIO.LOW)
+    GPIO.digitalWrite(RELAY4, GPIO.LOW)
 
 # loop function is repeatedly called by WebIOPi 
 def loop():
-    # retrieve current datetime
-    now = datetime.datetime.now()
-
-    # toggle light ON all days at the correct time
-    if ((now.hour == HOUR_ON) and (now.minute == 0) and (now.second == 0)):
-        if (GPIO.digitalRead(LIGHT) == GPIO.LOW):
-            GPIO.digitalWrite(LIGHT, GPIO.HIGH)
-
-    # toggle light OFF
-    if ((now.hour == HOUR_OFF) and (now.minute == 0) and (now.second == 0)):
-        if (GPIO.digitalRead(LIGHT) == GPIO.HIGH):
-            GPIO.digitalWrite(LIGHT, GPIO.LOW)
 
     # gives CPU some time before looping again
     webiopi.sleep(1)
 
 # destroy function is called at WebIOPi shutdown
 def destroy():
-    GPIO.digitalWrite(LIGHT, GPIO.LOW)
+    GPIO.digitalWrite(RELAY1, GPIO.LOW)
+    GPIO.digitalWrite(RELAY2, GPIO.LOW)
+    GPIO.digitalWrite(RELAY3, GPIO.LOW)
+    GPIO.digitalWrite(RELAY4, GPIO.LOW)
+
+# set the control mode
+@webiopi.macro
+def setMode(state):
+    if(state == "1"):
+        GPIO.digitalWrite(RELAY1, GPIO.LOW)
+        GPIO.digitalWrite(RELAY2, GPIO.LOW)
+        GPIO.digitalWrite(RELAY3, GPIO.LOW)
+        GPIO.digitalWrite(RELAY4, GPIO.LOW)
+        return "MODE 1 \n M1 = Tiller (UP/DOWN) \n M2 = Motor (LEFT/RIGHT)"
+    elif (state == "2"):
+        GPIO.digitalWrite(RELAY1, GPIO.LOW)
+        GPIO.digitalWrite(RELAY2, GPIO.HIGH)
+        GPIO.digitalWrite(RELAY3, GPIO.HIGH)
+        GPIO.digitalWrite(RELAY4, GPIO.LOW)
+        return "MODE 2 \n M1 = Hopper (UP/DOWN) \n M2 = Foot (LEFT/RIGHT)"
+    else:
+        GPIO.digitalWrite(RELAY1, GPIO.HIGH)
+        GPIO.digitalWrite(RELAY2, GPIO.LOW)
+        GPIO.digitalWrite(RELAY3, GPIO.LOW)
+        GPIO.digitalWrite(RELAY4, GPIO.HIGH)
+        return "MODE 3 \n M1 = Spike (UP/DOWN) \n M2 = Beak (LEFT/RIGHT)"
+
+# motor control functions
+def sendcommand(address,command):
+    global checksum
+    checksum = address
+    port.write(chr(address));
+    checksum += command
+    port.write(chr(command));
+    return;
+
+def writebyte(val):
+    global checksum
+    checksum += val
+    return port.write(struct.pack('>B',val));
 
 @webiopi.macro
-def getLightHours():
-    return "%d;%d" % (HOUR_ON, HOUR_OFF)
+def DriveM1(val):
+    sendcommand(128,6)
+    writebyte(val)
+    writebyte(checksum&0x7F);
+    if(vall == 127):
+        return "Motor1 forward..."
+    elif(val == 0):
+        return "Motor1 stopped..."
+    else:
+        return "Motor1 reverse..."
 
 @webiopi.macro
-def setLightHours(on, off):
-    global HOUR_ON, HOUR_OFF
-    HOUR_ON = int(on)
-    HOUR_OFF = int(off)
-    return getLightHours()
-
-# toggle light state with "0" and "1"
-@webiopi.macro
-def toggleLight(state):
-    # GPIO.digitalWrite(LIGHT, GPIO.HIGH)
-    if(state == "0"):
-        GPIO.digitalWrite(LIGHT, GPIO.LOW)
-    elif (state == "1"):
-        GPIO.digitalWrite(LIGHT, GPIO.HIGH)
-    return "%d;%d" % (HOUR_ON, HOUR_OFF)
+def DriveM2(val):
+    sendcommand(128,7)
+    writebyte(val)
+    writebyte(checksum&0x7F);
+    if(vall == 127):
+        return "Motor2 forward..."
+    elif(val == 0):
+        return "Motor2 stopped..."
+    else:
+        return "Motor2 reverse..."
+    
